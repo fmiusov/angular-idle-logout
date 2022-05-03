828a8c3b51d9ed3eeb72939e7dfa4eeb702bab9c
Jayson Duff <jayson.duff@gsgus.com>
2022-05-03 02:56:21 -0400

    ts errors in material, issues with modal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, ViewChild, TemplateRef, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  Inject,
+  ViewChild,
+  TemplateRef,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core'; // idle libraries brought in here
@@ -8,25 +15,31 @@ import { BsModalService } from 'ngx-bootstrap/modal'; // modal service here
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { AppService } from './_services/app.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   idleState = 'Not started.';
   timedOut = false;
   lastPing?: Date = null;
   title = 'angular-idle-timeout';
 
-  public modalRef: BsModalRef;
+  public modalRef: MatDialogRef;
 
   @ViewChild('childModal', { static: false }) childModal: ModalDirective;
 
-  constructor(private idle: Idle, private keepalive: Keepalive, 
-    private router: Router, private modalService: BsModalService, private appService: AppService) {
+  constructor(
+    private idle: Idle,
+    private keepalive: Keepalive,
+    private router: Router,
+    private dialog: MatDialog,
+    private appService: AppService
+  ) {
     // sets an idle timeout of 5 seconds, for testing purposes.
     idle.setIdle(5);
     // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
@@ -34,44 +47,48 @@ export class AppComponent {
     // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
     idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
 
-    idle.onIdleEnd.subscribe(() => { 
-      this.idleState = 'No longer idle.'
+    idle.onIdleEnd.subscribe(() => {
+      this.idleState = 'No longer idle.';
       console.log(this.idleState);
       this.reset();
     });
-    
+
     idle.onTimeout.subscribe(() => {
-      this.childModal.hide();
+      // this.childModal.hide();
+      this.dialog.open(TimeOutModal, {
+        width: 
+      })
       this.idleState = 'Timed out!';
       this.timedOut = true;
       console.log(this.idleState);
       this.router.navigate(['/']); // close down the modal and send the user back to the login page
     });
-    
+
     idle.onIdleStart.subscribe(() => {
-        this.idleState = 'You\'ve gone idle!'
-        console.log(this.idleState);
-        this.childModal.show();
+      this.idleState = "You've gone idle!";
+      console.log(this.idleState);
+      this.childModal.show();
     });
-    
+
     idle.onTimeoutWarning.subscribe((countdown) => {
-      this.idleState = 'You will time out in ' + countdown + ' seconds!'
+      this.idleState = 'You will time out in ' + countdown + ' seconds!';
       console.log(this.idleState);
     });
 
     // sets the ping interval to 15 seconds
     keepalive.interval(15);
 
-    keepalive.onPing.subscribe(() => this.lastPing = new Date());
+    keepalive.onPing.subscribe(() => (this.lastPing = new Date()));
 
-    this.appService.getUserLoggedIn().subscribe(userLoggedIn => { // need to find out if we have same flag for user logged in
+    this.appService.getUserLoggedIn().subscribe((userLoggedIn) => {
+      // need to find out if we have same flag for user logged in
       if (userLoggedIn) {
-        idle.watch()
+        idle.watch();
         this.timedOut = false;
       } else {
         idle.stop();
       }
-    })
+    });
 
     // this.reset();
   }
@@ -96,5 +113,4 @@ export class AppComponent {
     this.appService.setUserLoggedIn(false);
     this.router.navigate(['/']);
   }
-
 }
